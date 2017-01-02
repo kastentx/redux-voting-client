385107a2074d5fac9c9a3b90d6f23ba115df759f
Nick Kasten <kastencode@gmail.com>
2017-01-01 20:36:05 -0600

    first route with react-router now working

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
+import App from './components/App'
 import Voting from './components/Voting'
-
-const pair = ['Hackers', 'Fast & Furious']
 
 const routes = <Route component={App}>
   <Route path='/' component={Voting} />
   </Route>
+
 ReactDOM.render(
-  <Voting pair={pair} winner='Hackers' />,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 )
